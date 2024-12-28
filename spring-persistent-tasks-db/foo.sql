c54dfb47bf1d18b8baa2f1532d8ad4a92242b044
Paul Sterl <paul@sterl.org>
2024-12-28 12:23:23 +0100

    rename folders

--- spring-persistent-tasks-db/foo.sql
+++
@@ -1,5 +0,0 @@
-use master;
-drop SEQUENCE if EXISTS dbo.seq_PT_triggers_history_entries;
-drop SEQUENCE if EXISTS dbo.vehicle_seq;
-drop SEQUENCE if EXISTS dbo.engine_seq;
-drop SEQUENCE if EXISTS dbo.seq_PT_task_triggers;
