f8717f366f6d9531fc034fa239f99c3a9e4aeab4
Paul Sterl <paul@sterl.org>
2025-01-06 19:08:48 +0100

    cleanup

--- core/src/main/java/org/sterl/spring/persistent_tasks/api/TriggerKey.java
+++ core/src/main/java/org/sterl/spring/persistent_tasks/api/TriggerKey.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.lang.Nullable;
 
 import lombok.AllArgsConstructor;
