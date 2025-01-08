2e16bb0d9d27215fd0d919b0877834bde060ec62
Paul Sterl <paul@sterl.org>
2025-01-08 15:54:03 +0100

    cleanup

--- core/src/main/java/org/sterl/spring/persistent_tasks/history/repository/TriggerHistoryLastStateRepository.java
+++ core/src/main/java/org/sterl/spring/persistent_tasks/history/repository/TriggerHistoryLastStateRepository.java
@@ -1,13 +1,5 @@
 package org.sterl.spring.persistent_tasks.history.repository;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.sterl.spring.persistent_tasks.api.HistoryOverview;
-import org.sterl.spring.persistent_tasks.history.model.TriggerHistoryDetailEntity;
 import org.sterl.spring.persistent_tasks.history.model.TriggerHistoryLastStateEntity;
 
 public interface TriggerHistoryLastStateRepository extends HistoryTriggerRepository<TriggerHistoryLastStateEntity> {
