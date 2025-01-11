bab820825b74deb4a064917970628d2d9dd0ed9c
Paul Sterl <paul@sterl.org>
2025-01-11 18:53:39 +0100

    adjusted test

--- ui/test/shared/date.util.test.ts
+++ ui/test/shared/date.util.test.ts
@@ -3,7 +3,7 @@ import { formatMs } from "@src/shared/date.util";
 
 describe("Date Util Test", () => {
     it("formatMs", () => {
-        expect(formatMs(undefined)).toBe("");
+        expect(formatMs(undefined)).toBe("-");
         expect(formatMs(9000)).toBe("9000ms");
         expect(formatMs(59*1000 + 50)).toBe("59s 50ms");
         expect(formatMs(7*60*1000 + 50)).toBe("7min 0s");
