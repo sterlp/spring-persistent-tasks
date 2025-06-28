03797f9cc19402f016856a42657f603f5b826e47
Paul Sterl <paul@sterl.org>
2025-06-28 13:42:22 +0200

    add publish

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,40 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/task-ui',
+    base: "/task-ui",
     build: {
-        outDir: './dist/static/task-ui',
-        emptyOutDir: true
+        outDir: "./dist/static/task-ui",
+        emptyOutDir: true,
     },
     resolve: {
         alias: {
-            '@src': path.resolve(__dirname, './src'),
-        }
+            "@src": path.resolve(__dirname, "./src"),
+        },
     },
     test: {
-        environment: 'jsdom',
+        environment: "jsdom",
         globals: true,
-        setupFiles: './test/setup.ts',
-        css: true
+        setupFiles: "./test/setup.ts",
+        css: true,
     },
     server: {
         proxy: {
-            '/api': {
-                target: 'http://localhost:8080',
+            "/api": {
+                target: "http://localhost:8080",
                 changeOrigin: true,
                 secure: false,
                 ws: true,
             },
-            '/spring-tasks-api': {
-                target: 'http://localhost:8080',
+            "/spring-tasks-api": {
+                target: "http://localhost:8080",
                 changeOrigin: true,
                 secure: false,
                 ws: true,
-            }
-        }
-    }
-})
+            },
+        },
+    },
+});
